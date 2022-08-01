22c6e0df20a31535822d4b523f1ab0b996a073a7
sujaloo7 <sujal.ktech@gmail.com>
2022-08-01 17:22:43 +0530

    Wi-Fi Publictaion 📝

---
+++ Frontend/Components/Navbar.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wi-Fi Publictaion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <style>
+    /* From uiverse.io by @alexroumi */
+/* From uiverse.io by @satyamchaudharydev */
+.login {
+  --primary-color: #000;
+  --secondary-color: #fff;
+  --hover-color: #111;
+  --arrow-width: 10px;
+  --arrow-stroke: 2px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 35px;
+  color: var(--secondary-color);
+  padding: 1em 1.8em;
+  background: var(--primary-color);
+  display: flex;
+  transition: 0.2s background;
+  align-items: center;
+  gap: 0.6em;
+  font-weight: bold;
+}
+
+.login .arrow-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login .arrow {
+  margin-top: 1px;
+  width: var(--arrow-width);
+  background: var(--primary-color);
+  height: var(--arrow-stroke);
+  position: relative;
+  transition: 0.2s;
+}
+
+.login .arrow::before {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  border: solid var(--secondary-color);
+  border-width: 0 var(--arrow-stroke) var(--arrow-stroke) 0;
+  display: inline-block;
+  top: -3px;
+  right: 3px;
+  transition: 0.2s;
+  padding: 3px;
+  transform: rotate(-45deg);
+}
+
+.login:hover {
+  background-color: var(--hover-color);
+}
+
+.login:hover .arrow {
+  background: var(--secondary-color);
+}
+
+.login:hover .arrow:before {
+  right: 0;
+}
+  </style>
+
+  <nav class="navbar navbar-expand-lg bg-white shadow-sm p-3">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"> <img src="./images/logo.png" alt="Logo"> </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+       
+        
+      </ul>
+      <button class=" login p-0 px-4 py-2">
+        <a href="./Authentication/login.php" class="text-decoration-none text-light mb-1">
+    Sign In
+    </a>
+    <div class="arrow-wrapper">
+        <div class="arrow"></div>
+
+    </div>
+    
+</button>
+    </div>
+  </div>
+</nav>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
